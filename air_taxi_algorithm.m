2292e9111d88d13ebbd049688e9012cb1e7e988a
Chris Moneyron <cmoneyro@purdue.edu>
2019-04-21 22:49:55 -0400

    Consider recharge times

--- air_taxi_algorithm.m
+++ air_taxi_algorithm.m
@@ -24,36 +24,71 @@ clc;
 % n = 10 -> ~1.5 seconds runtime (brute force)
 % n = 11 -> ~12 seconds runtime (brute force)
 % n = 12 -> ~128 seconds runtime (brute force)
-n = 5; % Number of travel routes including current one
-A = randi(100, n); % Enter condensed graph adjacency matrix here
+n = 4; % Number of travel routes including current one
+A = randi(21, n); % Enter condensed graph adjacency matrix here
 % n = size(A, 1) % Command to get size of input adjacency matrix
 A(:, 1) = 0; % Cannot travel to current node
 A = A - diag(diag(A)); % No distance to same node
+curr_weight = randi(21); % Weight of current route (for computing recharges)
+recharge_time = 5;
+range = 21;
+curr_weight = 4;
+A = [0 14 10 9; 0 0 13 14; 0 11 0 10; 0 19 8 0];
 
 % Greedy algorithm if n > 11
 if n > 11
-    min_path = zeros(1, n);
-    min_path(1) = 1;
-    min_weight = 0;
+    min_path = 1;
+    min_weight = curr_weight;
     
     % Start node is always 1 so unvisited ones are remaining nodes
     curr_node = 1;
     unvisited_nodes = 2:n;
+    visited_nodes = 1;
+    
+    weight_since_recharge = curr_weight;
+    recharges = 0;
     
     % Choose minimum link weight until all nodes are visited
     while ~isempty(unvisited_nodes)
         
         [min_local_weight, min_idx] = min(A(curr_node, unvisited_nodes));
+        
+        % Impossible route if path weight is greater than range
+        if min_local_weight > range
+            if curr_node == 1
+                min_path = zeros(1, size(A, 1));
+                min_weight = realmax;
+                break
+            end
+            unvisited_nodes = sort([unvisited_nodes, min_path(end)]);
+            min_path(end) = [];
+            A(min_path(end), curr_node) = realmax;
+            curr_node = min_path(end);
+            continue
+        end
+        
+        % Track total weight since last recharge
+        weight_since_recharge = weight_since_recharge + min_local_weight;
+        
+        % Recharge for recharge time if weight since last recharge
+        % is greater than the range of the air taxi
+        if weight_since_recharge > range
+            recharges = recharges + 1;
+            min_local_weight = min_local_weight + (recharge_time * fix(weight_since_recharge/range));
+            weight_since_recharge = mod(weight_since_recharge, range);
+        end
+        
         min_weight = min_weight + min_local_weight;
         
         % Redefine current node based on index of minimum unvisited nodes
         curr_node = unvisited_nodes(min_idx);
         
-        % Remove current node from unvisited nodes array
+        % Remove current node from unvisited nodes array and add to visited
+        % array
         unvisited_nodes(unvisited_nodes == curr_node) = [];
         
         % Add current node to the minimum path array
-        min_path(n - size(unvisited_nodes, 2)) = curr_node;
+        min_path = [min_path, curr_node];
         
     end
     
@@ -61,7 +96,7 @@ if n > 11
     min_path_str = sprintf('%d -> ', min_path(1:(n-1)));
     min_path_str = strcat(min_path_str, sprintf(' %d', min_path(n)));
     
-% Brute force if n <= 11 (run once large network gets down to 10 requests)
+% Brute force if n <= 11 (run once large network gets down to 11 requests)
 else
     % Find all possible paths through all nodes
     % Add 1 since first node not in permutation array
@@ -69,21 +104,49 @@ else
     
     % Calculate total link weight for each possible path
     if size(all_paths', 1) > 0
+        
         min_weight = realmax;
+        
         for path = all_paths'
+            
             % First link is from node 1 to the first node in the path vector
-            tot_weight = A(1, path(1));
-
-            for j = 1:(n - 2) % Iterate only until n-2 b/c last node is only destination
-
+            path = [1; path];
+            tot_weight = curr_weight;
+            
+            weight_since_recharge = curr_weight;
+            recharges = 0;
+            
+            for j = 1:(n - 1) % Iterate only until n-2 b/c last node is only destination
+                path_weight = A(path(j), path(j+1));
+                
+                % Impossible route if path weight is greater than range
+                if path_weight > range
+                    tot_weight = realmax;
+                    if j == 1
+                        min_path = zeros(1, size(A, 1));
+                    end
+                    break
+                end
+                
+                % Track total weight since last recharge
+                weight_since_recharge = weight_since_recharge + path_weight;
+                
+                % Recharge for recharge time if weight since last recharge
+                % is greater than the range of the air taxi
+                if weight_since_recharge > range
+                    recharges = recharges + 1;
+                    path_weight = path_weight + (recharge_time * fix(weight_since_recharge/range));
+                    weight_since_recharge = mod(weight_since_recharge, range);
+                end
+                
                 % Add A(j,j+1) to tot_weight variable; j=origin, j+1=destination
-                tot_weight = tot_weight + A(path(j), path(j+1));
-
+                tot_weight = tot_weight + path_weight;
+                
             end
 
             if tot_weight < min_weight
                 min_weight = tot_weight;
-                min_path = [1, path']; % Add current node (1) to beginning of min path
+                min_path = path;
             end
 
         end
